b0bb3bbe2184af82482602afb07b94a11e258cc9
elguille2 <guillermo2pacheco@gmail.com>
2023-11-12 14:43:11 +0100

    feat: Add census export funcionality

--- decide/census/models.py
+++ decide/census/models.py
@@ -7,3 +7,7 @@ class Census(models.Model):
 
     class Meta:
         unique_together = (('voting_id', 'voter_id'),)
+
+    # String representation for easy identification in admin views
+    def __str__(self):
+        return f"Census - Voting ID: {self.voting_id}, Voter ID: {self.voter_id}"
